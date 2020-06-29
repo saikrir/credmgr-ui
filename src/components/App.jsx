e633e3086412afed2465ea8324de67e84b51bb9f
Sai Katterishetty <rao.saikrishna@gmail.com>
2020-06-28 22:27:34 -0500

    Added App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to React App</h1>
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
