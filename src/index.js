3d23b0dc7c5549b68f2bc2381bd5d1ffbb4dc699
saikrir <rao.saikrishna@gmail.com>
2020-07-26 15:30:16 -0500

    Switched to Semantic React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import App from "./components/App";
-import ReactDOM from "react-dom";
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-if (module.hot) {
-  module.hot.accept();
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
